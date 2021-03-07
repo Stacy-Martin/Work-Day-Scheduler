eff72977ec900e7772986a8920fecf5dbc2cca28
Stacy Martin <sbrown1031@gmail.com>
2021-03-06 22:21:43 -0600

    finished table

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,17 +3,95 @@
 // form is created ...???
 
 // this variable sets where the time is displayed in the HTML
-var timeDisplayEl = $('#time-display');
-
+const timeDisplayEl = $('#time-display');
+const rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
 // handle displaying the time
+const container = $('.container')
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    
     timeDisplayEl.text(rightNow);
 }
 
 setInterval(displayTime, 1000);
 
 // need to create a variable and function to create one hour intervals (10) from 8am - 5pm
+createRow(24); //numRow = 10
+function createRow(numRow) {
+    for(let i=0; i<numRow; i++) {
+            //wrapper that is the row
+    //create a row with textarea, hour, saveBtn
+        let time = i + 8;
+        let wrapper = $("<div>") //$("div")this selects all divs
+        wrapper.attr('id', time +"-hour") //set id of wrapper to i + -hour (8-hour)
+        wrapper.attr('class', 'row time-block')
+        let hourBlock = $('<h5>');
+        hourBlock.attr('class', 'hour col-md-1');
+        hourBlock.text(time +":00");
+        wrapper.append(hourBlock);
+
+        let textArea = $("<textarea>");
+        textArea.attr('class', 'description future col-md-10');
+        wrapper.append(textArea);
+
+        let saveBtn = $("<button>");
+        saveBtn.attr('class', 'saveBtn col-md-1');
+        saveBtn.attr('id', time+'-btn')
+
+        wrapper.append(saveBtn);
+        
+        container.append(wrapper)
+    }
+}
+
+checkTime();
+function checkTime(){
+    const rows = $('.time-block');
+    console.log(rows);
+
+    for(let i = 0; i < rows.length; i++){
+        if (rows < rightNow) {
+            $(this).addClass("past");
+        }
+      
+        else if (rows == rightNow) {
+            $(this).addClass("present")
+        }
+
+        else {
+            $(this).addClass("future")
+        }
+    }
+}
+        //if(timeBlockID < rightNow){
+            //past
+        //}
+        //else if(timeBlockID == rightNow){
+            //present
+        //}
+        //else{
+            //future
+        //}
+
+
+function storeForm(event){
+    //accessing Local Storage whoooooooo
+    //setItem("time-hour", valueFromForm)
+    //textArea.val() -> gets the value from a textarea
+    //look up how to parse id from element
+    console.log(event.target)
+}
+
+function getForm(){
+    //gets stuff from LocalStorage on load up
+    //get item also we need another for-loop thru timeblocks
+}
+
+$(document).ready(
+    //JQuery's version of addEventListener('click', ...)
+    //this is event delegation having one event listener looking for saveBtn clicks
+        $(".saveBtn").on("click", storeForm)
+    )
+
 
 
 // want to manipulate the DOM using jquery / JS loop
@@ -36,6 +114,13 @@ setInterval(displayTime, 1000);
 // ? use jquery or javascript for this ?
 
 // click in a timeblock to add user schedule (enter an event) in the textarea
+
+// .saveBtn --> user event is saved to local storage, such that when page is refreshed the user's schedule persists 
+
+
+
+// $(this) takes thre current DOM element and turns it into a jQuery object
+// thus exposing it to JQuery's methods 
 
 // .saveBtn --> user event is saved to local storage, such that when page is refreshed the user's schedule persists 
 $(".saveBtn").on("click", function(){
